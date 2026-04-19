d98345d903b3329175d85570570b5adcab08f8ee
MangoDuck26 <dannytraylor326@gmail.com>
2026-04-19 15:27:52 -0400

    Extend the Station model for Firestore metadata

--- src/main/java/com/laughingalpaca/bikeviewapp/Model/Station.java
+++ src/main/java/com/laughingalpaca/bikeviewapp/Model/Station.java
@@ -11,6 +11,44 @@ public class Station {
     private String borough;
     private String zip_code;
     private Instant last_updated;
+    public String getStation_id() {
+        return station_id;
+    }
 
+    public void setStation_id(String station_id) {
+        this.station_id = station_id;
+    }
+
+    public String getStation_name() {
+        return station_name;
+    }
+
+    public void setStation_name(String station_name) {
+        this.station_name = station_name;
+    }
+
+    public MapPoint getLocation() {
+        return location;
+    }
+
+    public void setLocation(MapPoint location) {
+        this.location = location;
+    }
+
+    public int getEstimated_bike_count() {
+        return estimated_bike_count;
+    }
+
+    public void setEstimated_bike_count(int estimated_bike_count) {
+        this.estimated_bike_count = estimated_bike_count;
+    }
+
+    public String getBorough() {
+        return borough;
+    }
+
+    public void setBorough(String borough) {
+        this.borough = borough;
+    }
 
 }
