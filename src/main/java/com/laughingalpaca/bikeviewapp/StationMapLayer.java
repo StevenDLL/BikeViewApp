ebabe8a974b1b8f4da400f945def0412a9961a9e
Steven <33363109+StevenDLL@users.noreply.github.com>
2026-05-07 15:20:00 -0400

    Added rounding of station latitude and longitude for visual clarity(does not affect backend data, we still have full location on our end)

--- src/main/java/com/laughingalpaca/bikeviewapp/StationMapLayer.java
+++ src/main/java/com/laughingalpaca/bikeviewapp/StationMapLayer.java
@@ -14,6 +14,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -45,8 +47,8 @@ public class StationMapLayer extends MapLayer {
                     pane.setVisible(true);
                     labels[0].setText(station.getStation_id());
                     labels[1].setText(station.getStation_name());
-                    labels[2].setText(String.valueOf(station.getLocation().getLatitude()));
-                    labels[3].setText(String.valueOf(station.getLocation().getLongitude()));
+                    labels[2].setText(String.valueOf(truncateCoordinate(station.getLocation().getLatitude())));
+                    labels[3].setText(String.valueOf(truncateCoordinate(station.getLocation().getLongitude())));
                     labels[4].setText(String.valueOf(station.getEstimated_bike_count()));
                 }
 
@@ -76,11 +78,47 @@ public class StationMapLayer extends MapLayer {
 
     }
 
-    //TODO: Implement class constructor here that handles taking in a List<Station>
-    // and creating a dot on the map for each individual station, also make it so
-    // when a user clicks on said dot it will pop up with the information for that
-    // Station, users should then be able to click on the displayed bike count to
-    // see all bikes at the station with some information of the previous trip it took - Complete
+    public StationMapLayer(List<Station> stationList) {
+        for (Station station : stationList) {
+            Circle circle = new Circle(5, Color.web("#625bb2"));
+            circle.setStroke(Color.web("#2b2751"));
+            circle.setStrokeWidth(3);
+
+            circle.setOnMouseEntered(mouseEvent -> {
+                this.setCursor(Cursor.HAND);
+                circle.setFill(Color.web("#8f89d4"));
+                circle.setRadius(7);
+            });
+
+            circle.setOnMouseExited(mouseEvent -> {
+                this.setCursor(Cursor.DEFAULT);
+                circle.setFill(Color.web("#625bb2"));
+                circle.setRadius(5);
+            });
+
+            Tooltip tooltip = new Tooltip(station.getStation_name());
+            tooltip.setShowDelay(Duration.millis(100));
+            tooltip.setStyle(
+                    "-fx-background-color: #2b2751;" +
+                            "-fx-text-fill: white;" +
+                            "-fx-font-family: Arial;" +
+                            "-fx-font-size: 13px;" +
+                            "-fx-font-weight: bold;" +
+                            "-fx-border-color: #625bb2;" +
+                            "-fx-border-width: 1;" +
+                            "-fx-border-radius: 6;" +
+                            "-fx-background-radius: 6;" +
+                            "-fx-padding: 6 10 6 10;"
+            );
+
+            Tooltip.install(circle, tooltip);
+
+            map.put(station, circle);
+            this.getChildren().add(circle);
+
+        }
+    }
+
     @Override
     protected void layoutLayer() {
         // Iterate through all markers and update their screen position
@@ -95,6 +133,10 @@ public class StationMapLayer extends MapLayer {
                 node.setVisible(false);
             }
         });
+    }
+
+    protected static double truncateCoordinate(double value) {
+        return new BigDecimal(String.valueOf((value))).setScale(5, RoundingMode.DOWN).doubleValue();
     }
 }
 
